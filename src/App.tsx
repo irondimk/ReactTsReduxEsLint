960e0fc3f49aae5f88f79d78d53112d87961def3
Дмитрий Сорокин <irondimk@gmail.com>
2021-09-02 10:42:10 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './App.css';
+
+export default function App() {
+    return (
+        <div>
+            <div>
+                Configure React, Redux, React-Redux, TypeScript, ESLint, AXIOS,
+            </div>
+        </div>
+    );
+}
